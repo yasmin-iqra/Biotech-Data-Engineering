2b0b9ff89daa25a0af7ace3be4e5805ecc29fd3e
yasmin-iqra <iqramoni21@gmail.com>
2026-05-30 15:36:23 +0530

    Finished input exercise chapter

--- Basics.py
+++ Basics.py
@@ -1,1 +1,23 @@
 print("Hello Mumbai! Day 1 of my Data Engineering journey.")
+print('o-----')
+print('|||||')
+print('*'*10)
+Age=23
+Age=30
+rating=7.3
+name='Iqra'
+is_published=False
+print(Age)
+#A Teacher Named Nitesh Kumar.He's 32 Years Old and is a New Teacher
+#Variable
+full_name='Nitesh Kumar'
+age=32
+is_new=True
+name=input('What is your name? ')
+print('Hello '+name)
+name=input('What is your name? ')
+favorite_color=input("What is your favorite color? ")
+print(name + 'likes' + favorite_color)
+
+
+
